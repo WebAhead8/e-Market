f4fe5a0b9121a52aa9da9b5a9b40973a5b8b520c
Jihad Banna <super_banna@hotmail.com>
2021-02-17 15:57:01 +0200

    filters

--- e-market/src/App.js
+++ e-market/src/App.js
@@ -1,13 +1,30 @@
-
-import './App.css';
-
+import "./App.css";
+import React from "react";
+import PriceFilter from "./components/PriceFilter";
+import MarketList from "./components/marketList";
 function App() {
+  const [maxPrice, setMaxPrice] = React.useState(100);
+  const [minPrice, setMinPrice] = React.useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
+    <section className="filters">
+      <h1>e-Market</h1>
+      <h2> Filter By</h2>
+
+      <PriceFilter
+        maxPrice={maxPrice}
+        setMaxPrice={setMaxPrice}
+        minPrice={minPrice}
+        setMinPrice={setMinPrice}
+      />
+
+      <MarketList
+        maxPrice={maxPrice}
+        setMaxPrice={setMaxPrice}
+        minPrice={minPrice}
+        setMinPrice={setMinPrice}
+      />
+    </section>
   );
 }
 
