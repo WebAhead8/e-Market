88c042bed6bc76c82d3d84cc2ff320900ab28b4e
Jihad Banna <super_banna@hotmail.com>
2021-02-17 11:29:18 +0200

    fakeData

---
+++ e-market/src/data.js
@@ -0,0 +1,74 @@
+const data = [
+  {
+    id: 1,
+    name: "Milk",
+    description: "3% fat milk",
+    price: 6,
+    category: "Milk products",
+  },
+  {
+    id: 2,
+    name: "Lamb meat ",
+    description: "organic meat",
+    price: 70,
+    category: "Meat",
+  },
+  {
+    id: 3,
+    name: "Cola",
+    description: "1.5 liter soda drink",
+    price: 7,
+    category: "Drinks",
+  },
+  {
+    id: 4,
+    name: "Potato",
+    description: "original batata",
+    price: 4,
+    category: "Vegetables",
+  },
+  {
+    id: 5,
+    name: "Apple",
+    description: "One fresh apple from Hadabeh",
+    price: 11,
+    category: "Fruits",
+  },
+  {
+    id: 6,
+    name: "Kinder bueno",
+    description: "tasty and yummy chocolate bar ",
+    price: 5,
+    category: "Candies",
+  },
+  {
+    id: 7,
+    name: "Arabic bread",
+    description: "pack of ten ",
+    price: 10,
+    category: "Bread",
+  },
+  {
+    id: 8,
+    name: "Cashew ",
+    description: "100 gram ",
+    price: 17,
+    category: "Nuts",
+  },
+  {
+    id: 9,
+    name: "Vanish",
+    description: "2 kilo ",
+    price: 39,
+    category: "Cleaning products",
+  },
+  {
+    id: 10,
+    name: "Rice",
+    description: "5 kilo ",
+    price: 55,
+    category: "Staple food",
+  },
+];
+
+export default data;
